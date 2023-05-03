631aeeea0f716e873f0b0a189b858967f536b645
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-03 22:17:14 +0800

    Initial Commit

---
+++ Shared/__swift_tabviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swift_tabviewApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct __swift_tabviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
